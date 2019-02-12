292608b08d4c6f6ac0520096fc6dfe8f517f12ce
Keith Allan <keithallan01@gmail.com>
2019-02-12 22:10:31 +0000

    Books ArrayList added

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+    private ArrayList<Book> books;
+
+    public Book(String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.books = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public String getGenre(){
+        return this.genre;
+    }
+
+    public int bookCount(){
+        return this.books.size();   
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Lord Of The Rings", "Tolkien", "Fantasy");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("Lord Of The Rings", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor(){
+        assertEquals("Tolkien", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre(){
+        assertEquals("Fantasy", book.getGenre());
+    }
+
+    @Test
+    public void startsWithNoBooks(){
+        assertEquals(0, book.bookCount());
+    }
 }
